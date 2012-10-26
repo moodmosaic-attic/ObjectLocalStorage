be0f570a5ce8ddabbe52621f4d227a00a32a9ac1
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2012-10-27 00:50:07 +0300

    Initial commit.

---
+++ ObjectLocalStorage.UnitTest/ObjectLocalStorageTests.cs
@@ -0,0 +1,102 @@
+﻿using Ploeh.AutoFixture.Xunit;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+
+public class ObjectLocalStorageTests
+{
+    [Theory, AutoData]
+    public void GetWhenSetIsNotInvokedForObjectReturnsCorrectResult(
+        DateTime @object)
+    {
+        // Fixture setup
+        // Exercise system
+        object result = ObjectLocalStorage.Get(@object);
+        // Verify outcome
+        Assert.Null(result);
+        // Teardown
+    }
+
+    [Theory]
+    [InlineAutoData(-10)]
+    [InlineAutoData(2.0)]
+    [InlineAutoData("3")]
+    public void GetReturnsCorrectResult(
+        object @object,
+        string expected)
+    {
+        // Fixture setup
+        // Exercise system
+        ObjectLocalStorage.Set(@object, expected);
+        object result = ObjectLocalStorage.Get(@object);
+        // Verify outcome
+        Assert.Equal(expected, result);
+        // Teardown
+    }
+
+    [Theory, AutoData]
+    public void GetReturnsCorrectResultMultipleCall(
+        object @object, 
+        string dummy,
+        double expected)
+    {
+        // Fixture setup
+        Array.ForEach(new int[3], x => ObjectLocalStorage.Set(@object, dummy));
+        // Exercise system
+        ObjectLocalStorage.Set(@object, expected);
+        // Verify outcome
+        object result = ObjectLocalStorage.Get(@object);
+        // Teardown
+        Assert.Equal(expected, result);
+    }
+
+    [Theory, InlineData(null)]
+    public void GetForNullReferenceReturnsCorrectResult(object @object)
+    {
+        // Fixture setup
+        // Exercise system
+        object result = ObjectLocalStorage.Get(@object);
+        // Verify outcome
+        Assert.Null(result);
+        // Teardown
+    }
+
+    [Theory]
+    [InlineAutoData(-10)]
+    [InlineAutoData(2.0)]
+    [InlineAutoData("3")]
+    public void GetReturnsCorrectResultAsynchronousCall(
+        object @object,
+        string expected)
+    {
+        // Fixture setup
+        ObjectLocalStorage.Set(@object, expected);
+        object result = null;
+        // Exercise system
+        new Task(() => result = ObjectLocalStorage.Get(@object)).Start();
+        SpinWait.SpinUntil(() => result != null);
+        // Verify outcome
+        Assert.Equal(expected, result);
+        // Teardown
+    }
+
+    [Theory]
+    [InlineAutoData(-10)]
+    [InlineAutoData(2.0)]
+    [InlineAutoData("3")]
+    public void GetWhenSetIsInvokedForDifferentObjectReturnsCorrectResult(
+        object dummy,
+        object @object,
+        string expected)
+    {
+        // Fixture setup
+        ObjectLocalStorage.Set(dummy, expected);
+        // Exercise system
+        object result = ObjectLocalStorage.Get(@object);
+        // Verify outcome
+        Assert.Null(result);
+        // Teardown
+    }
+}
---
+++ ObjectLocalStorage/ObjectLocalStorage.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.CompilerServices;
+
+/// <summary>
+/// Allows to store an arbitrary piece of data to objects.
+/// </summary>
+public static class ObjectLocalStorage
+{
+    private static readonly ConditionalWeakTable<object, object> cwt =
+        new ConditionalWeakTable<object, object>();
+
+    /// <summary>
+    /// Removes the associated data for this object from the store (if exists)
+    /// and then associates the data with this object.
+    /// </summary>
+    /// <param name="object">The object that we would like to associate an 
+    /// arbitrary piece of data.
+    /// </param>
+    /// <param name="value">An arbitrary piece of data.</param>
+    public static void Set(object @object, object value)
+    {
+        cwt.Remove(@object);
+        cwt.Add(@object, value);
+    }
+
+    /// <summary>
+    /// Gets the data associated for a given object.
+    /// </summary>
+    /// <param name="object">The object that we have previously associated 
+    /// some data.
+    /// </param>
+    /// <returns></returns>
+    public static object Get(object @object)
+    {
+        if (@object == null)
+            return null;
+
+        object value;
+        cwt.TryGetValue(@object, out value);
+        return value;
+    }
+}
